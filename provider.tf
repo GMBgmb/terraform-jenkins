63775181cafdb95706f4bbf3469d9e630cb78099
GMBgmb <94504796+GMBgmb@users.noreply.github.com>
2022-04-28 23:06:58 -0400

    7

--- provider.tf
+++ provider.tf
@@ -8,7 +8,7 @@ terraform{
     bucket = "javahomegandeu-tf-1212" #Defaulbuketname
     key = "terraform.tfstate"
     region = "us-east-1"
-    dynamodb_table = "terraformstate" #default table name #LockID key should be for the table should not change
+    dynamodb_table = "terraformstate" #default table name #LockID the key should be for the table should not change
     
   }
 }
