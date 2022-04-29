47aa2281ec3f133f5a0dad551cca6e424a6b3192
GMBgmb <94504796+GMBgmb@users.noreply.github.com>
2022-04-28 22:11:13 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,6 +8,7 @@ terraform{
     bucket = "javahomegandeu-tf-1212"
     key = "terraform.tfstate"
     region = "us-east-1"
+    dynamodb_table = "terraformstate" #LockID Lokxid should not change
     
   }
 }
